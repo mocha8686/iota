3341e6d6e3f8ea7f3f53e6f1ebbb80526c57ba18
mocha8686 <jericho.paulino@yahoo.com.ph>
2024-04-09 16:00:57 -0700

    update db

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
 	id: text('id').notNull().primaryKey(),
@@ -8,8 +8,31 @@ export const users = sqliteTable('users', {
 
 export const sessions = sqliteTable('sessions', {
 	id: text('id').notNull().primaryKey(),
-	userId: text('userId')
+	userId: text('user_id')
 		.notNull()
 		.references(() => users.id),
 	expiresAt: integer('expires_at').notNull(),
 });
+
+export const characters = sqliteTable('characters', {
+	id: integer('id').notNull().primaryKey(),
+	name: text('name').notNull(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => users.id),
+	inventory: text('inventory', { mode: 'json' }).default('[]'),
+});
+
+export const items = sqliteTable(
+	'items',
+	{
+		id: integer('item_id').notNull(),
+		characterId: integer('character_id')
+			.notNull()
+			.references(() => characters.id),
+		quantity: integer('quantity').notNull().default(1),
+	},
+	(t) => ({
+		pk: primaryKey({ columns: [t.characterId, t.id] }),
+	}),
+);
