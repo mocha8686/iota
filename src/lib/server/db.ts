23f5307435b80a6c21a427406e9e59765225a043
mocha8686 <jericho.paulino@yahoo.com.ph>
2024-04-20 09:47:18 -0700

    remove wtihCharacter

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,6 @@
 import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from '$env/static/private';
 import { createClient } from '@libsql/client';
-import { eq } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/libsql';
-import { characters } from './schema';
 
 const conn = createClient({
 	url: TURSO_DATABASE_URL,
@@ -10,13 +8,3 @@ const conn = createClient({
 });
 
 export const db = drizzle(conn);
-
-export const withCharacter = (userId: string) =>
-	db
-		.$with('character')
-		.as(
-			db
-				.select({ id: characters.id })
-				.from(characters)
-				.where(eq(characters.userId, userId)),
-		);
